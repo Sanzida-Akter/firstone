56a7ccca96d5253d5b70fdcae8b90ae7d6302277
Sanzidamasvi <sanzida546@gmail.com>
2022-08-28 14:41:40 +0600

    FirstCommit

---
+++ seventh.js
@@ -0,0 +1,38 @@
+// function messagebutton1(){
+//     alert("Clicked Button 1");
+// }
+
+// function messagebutton2(){
+//     alert("Clicked Button 2");
+// }
+
+// function clickParagraph(){
+//     alert("Clicked the Paragraph()");
+// }
+
+ //using query selector for onclick
+// function messagebutton1(){
+//     var paraVar=document.querySelector("#paraId");
+//     paraVar.innerHTML="You have clicked the button 1";
+// }
+
+// function messagebutton2(){
+//     var paraVar=document.querySelector("#paraId");
+//     paraVar.innerHTML="You have clicked the button 2";
+// }
+
+//image click functions
+
+// var paraVar=document.querySelector("#imageId");
+
+//    // paraVar.src="images/planne.jpg";
+// function previous(){
+//     paraVar.src="images/car.JPG";
+// }
+// function next(){
+//     for(i=1;i<=3;i++){
+//     paraVar.src[0]="images/flower.jpg";
+//     paraVar.src[1]="images/plane.jpg";-->need to practice in home
+//     paraVar.src[2]="images/car.JPG";
+//     }
+// }
